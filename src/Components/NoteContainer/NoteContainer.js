62eabf7e52b8b3c0456d051194bda32fb1103163
Anish Kumar <94281610+anishkumar28@users.noreply.github.com>
2023-05-06 00:27:06 +0530

    Adding all files

---
+++ src/Components/NoteContainer/NoteContainer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Note from "../Note/Note";
+import "./NoteContainer.css"
+
+function NoteContainer(props) {
+
+  const reverArray = (arr) => {
+    const array = [];
+
+    for (let i = arr.length - 1; i >= 0; --i){
+      array.push(arr[i]);
+    }
+
+    return array;
+  };
+
+  const notes = reverArray(props.notes);
+
+
+  return (
+      <div className="note-container">
+        <h1>Lekh</h1>
+        <div className="note-container-notes custom-scroll">
+
+           {
+              notes?.length>0 ? notes.map((item)=><Note
+              key={item.id}
+              note={item}
+              deleteNote={props.deleteNote}
+              updateText={props.updateText}
+             />) : ""
+           }
+
+        </div>
+    </div>
+   );
+}
+
+export default NoteContainer;
